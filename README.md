398f649bf5d7a2db0066bdc13531416982d5064b
John Hofrichter <john.hofrichter@gmail.com>
2015-04-14 18:01:26 -0400

    added middleware

--- README.md
+++ README.md
@@ -11,4 +11,17 @@ ES6 is required for koa, so make sure a supporting version of node is installed
 
 Run the server with the `--harmony` flag to allow ES6 functionality
 
-- `$ node --harmony app`
+- `$ node --harmony --use-strict app`
+  - OR `$ npm start` (requires `nodemon`)
+
+## Dev tools
+
+If using sublime, install [Java​Script​Next - ES6 Syntax](https://packagecontrol.io/packages/JavaScriptNext%20-%20ES6%20Syntax) and set it as the default JS highlighter
+
+## Routes
+
+`/` - hellow world
+
+`/echo` - echo's the query param `say`
+
+`/authenticated` - prints `YES` or `NO` depending on whether or not `_authenticated_` is in the query string
--- app.js
+++ app.js
@@ -1,8 +1,106 @@
-var koa = require('koa');
-var app = koa();
+let koa   = require('koa');
+let route = require('koa-route');
+let colors = require('colors');
 
-app.use(function *(){
-  this.body = 'Hello World';
+/////////////////////////////////////////////////////////////////////////
+//
+// Initialization
+//
+/////////////////////////////////////////////////////////////////////////
+
+let app = koa();
+
+app.use(function *(next) {
+  // this => [
+  //   'request', // koa request
+  //   'response', // koa response
+  //   'app', // app instance
+  //   'req', // node req
+  //   'res', // node res
+  //   'onerror',
+  //   'originalUrl',
+  //   'cookies', // you guessed it
+  //   'accept'
+  // ]
+
+  console.log('  ' + (this.request.method).green + ' ' + (this.request.path).gray);
+  yield next;
 });
 
-app.listen(3000);
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Middleware
+//
+/////////////////////////////////////////////////////////////////////////
+
+
+let middleware = {
+  isAuth : function *() {
+    // ASYNC CODE GOES HERE
+    return ~this.request.querystring.indexOf('_authenticated_');
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Controllers
+//
+/////////////////////////////////////////////////////////////////////////
+
+
+let controllers = {
+
+  // Home
+  //
+  home : function *() {
+    this.body = 'Hello World'
+  },
+
+  // Echo
+  //
+  echo : function *() {
+    this.body = this.request.query.say || 'add a `say` query param to echo the value';
+  },
+
+  authenticated : function *() {
+    var isAuth = yield middleware.isAuth; // blocking async call. WAT.
+    this.body = isAuth ? 'YES' : 'NO';
+  }
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Routes
+//
+/////////////////////////////////////////////////////////////////////////
+
+
+// Home
+//
+app.use(route.get('/', controllers.home));
+
+
+// Echo
+//
+app.use(route.get('/echo', controllers.echo));
+
+
+// authenticated
+//
+app.use(route.get('/authenticated', controllers.authenticated));
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Finalize
+//
+/////////////////////////////////////////////////////////////////////////
+
+
+let port = 3000;
+app.listen(port);
+console.log('\nlistening on port ' + port + '...\n'); // string templating isn't implemented in harmonay at the time of writing :(
