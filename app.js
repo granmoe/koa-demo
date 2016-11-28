1b4755540fbc0880dfbf9c4fc16b8188ed9f3aaa
Matt Granmoe <matt.granmoe@bestbuy.com>
2016-11-27 22:10:52 -0600

    testing out koa

--- app.js
+++ app.js
@@ -10,9 +10,17 @@ let port = 3000;
 // Log out requests
 app.use(function *(next) {
   console.log('  ' + (this.request.method).green + ' ' + (this.request.path).gray);
+  console.log('1')
   yield next;
+  console.log('4')
 });
 
+// test
+app.use(function* (next) {
+  console.log('2')
+  yield next
+  console.log('3')
+})
 // Error Handler
 app.use(KError.handler);
 
